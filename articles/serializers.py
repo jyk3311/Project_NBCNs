585130b7783d370f2c97824a5e58665713c272f5
rabongee <rmarkdehdals@gmail.com>
2024-09-12 03:21:21 +0900

    feat: 게시물 상세 조회 기능 구현
    - 로그인된 유저가 아니라도 상세 게시물 조회 가능
    - Detail용 Serializer 추가적으로 구현

--- articles/serializers.py
+++ articles/serializers.py
@@ -6,3 +6,10 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('id', 'title', 'author', 'category', 'created_at')
+
+
+class ArticleDetailSerializer(ArticleSerializer):
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'content', 'author',
+                  'category', 'created_at', 'updated_at')
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,7 +6,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListCreateAPIView
 from .models import Article
-from .serializers import ArticleSerializer
+from .serializers import ArticleSerializer, ArticleDetailSerializer
 from .validators import validate_create
 
 
@@ -31,3 +32,15 @@ class ArticleListCreateAPIView(ListCreateAPIView):
 
         serializer = ArticleSerializer(article)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ArticleDetailAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        return get_object_or_404(Article, id=pk)
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleDetailSerializer(article)
+        return Response(serializer.data)
