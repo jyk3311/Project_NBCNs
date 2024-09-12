521a4ef987147090ff4d718d7fdc06ddf6ae4c35
KIM MIN JOO <rlaalswn0613@gmail.com>
2024-09-12 12:58:21 +0900

    feat: accounts 앱 기능 구현 완료 - 회원가입 및 회원탈퇴  - 회원가입 시, access_token 바로 발급  - 회원가입 시, username, password, email 유효성 검사  - 회원탈퇴 시, 비밀번호 인증 후 회원탈퇴 가능  - 회원탈퇴 시, soft_delete 비활성 계정으로 처리
    - 로그인
     - 아이디, 비밀번호 유효한지 확인 후 토큰 발급
    
    - 로그아웃
     - refresh_token 확인 후 로그아웃 처리함
     - 제출 시에 access_token timedelta 수정 필요함 (현재 days=1)
    
    - 프로필 조회 및 회원정보 수정
     - url로 접근할 때 존재하는 username으로 접근했는지 확인
     - 회원정보 수정 시, 이메일 or 비밀번호 수정 가능 (부분 수정)
     - 회원정보 수정 시, 접근한 user가 본인의 회원정보만  수정할 수 있음

---
+++ accounts/validators.py
@@ -0,0 +1,90 @@
+from .models import User
+from django.core.validators import validate_email
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ValidationError
+
+
+# 회원가입 시, validator
+def validate_signup(user, user_data):
+    err_msg = []
+
+    # validate_username
+    if username := user_data.get("username"):
+        if User.objects.filter(username=username).exists():
+            err_msg.append({"useranme":'이미 존재하는 아이디입니다.'})
+
+    # validate_password
+    password = user_data.get("password")
+    password2 = user_data.get("password2")
+    if not password == password2:
+        err_msg.append({"password":'비밀번호가 일치하지 않습니다.'})
+    else:
+        # settings에 있는 validator 사용한 유효성 검사 (비밀번호 자체에 대한 유효성 검사)
+        try:
+            validate_password(password, user)
+        except  ValidationError as e:
+            err_msg.append({"new_password": str(e)})
+
+
+    # validate_email
+    if email := user_data.get("email"):
+        if User.objects.filter(email=email).exists():
+            err_msg.append({"email":'이미 존재하는 이메일입니다.'})
+        else:
+            # email 형식 유효성 검사
+            try:
+                validate_email(email)
+            except:
+                err_msg.append({"email":'이메일 형식이 올바르지 않습니다.'})
+
+    if err_msg:
+        return False, err_msg
+    return True, err_msg
+                
+
+
+# 회원정보 수정 시, validator
+def validate_profile(user, update_data):
+    err_msg = []
+
+    # validation_email
+    if email := update_data.get("email"):
+        if User.objects.filter(email=email).exists():
+            err_msg.append({"email":'이미 존재하는 이메일입니다.'})
+        else:
+            try:
+                validate_email(email)
+            except:
+                err_msg.append({'email':'이메일 형식이 올바르지 않습니다.'})
+    # else:
+    #     err_msg.append({'email':'이메일이 없습니다.'})
+
+
+    # validation_password
+    new_password = update_data.get("new_password")
+    new_password2 = update_data.get("new_password2")
+    
+    # old_password DB에 있는지 확인
+    if old_password := update_data.get("old_password"):
+        if not check_password(old_password, user.password):
+                err_msg.append({"old_password":"비밀번호가 틀렸습니다."})
+        else:        
+            # validation_새 비밀번호가 이전 비밀번호와 일치하지 않아야 함
+            if old_password == new_password:
+                    err_msg.append({"new_password":"새로운 비밀번호를 입력해주세요."})
+
+        # validation_new_password 1,2 일치한지 확인
+        if new_password != new_password2 :
+            err_msg.append({"new_password":"비밀번호가 일치하지 않습니다."})
+        else:
+            # settings에 있는 validator 사용한 유효성 검사 (비밀번호 자체에 대한 유효성 검사사)
+            try:
+                validate_password(new_password, user)
+            except ValidationError as e:
+                err_msg.append({"new_password": str(e)})
+
+    # 에러메시지
+    if err_msg:
+        return False, err_msg 
+    return True, err_msg
