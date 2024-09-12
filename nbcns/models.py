3484e6decc13f9c77269cee334efe9ed79422def
leeyounghuncom <web3dao@kakao.com>
2024-09-13 07:19:14 +0900

    OpenAI settings.py 환경변수 추가 게시글 삭제 게시글 작성 목록 조회 상세 조회 북마크

--- nbcns/models.py
+++ nbcns/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings  # User 모델이 AUTH_USER_MODEL로 설정되어 있을 때 이를 사용
 
-# Create your models here.
+# NBCN 모델
+class NBCN(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField(max_length=500)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+# NBCN 북마크 모델
+class NBCN_Bookmark(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    NBCN = models.ForeignKey(NBCN, on_delete=models.CASCADE)  # NBCN 모델 클래스와 일치하도록 대소문자 맞춤
+
+    def __str__(self):
+        return f'{self.user.username} bookmarked {self.NBCN.title}'
--- spartanews/settings.py
+++ spartanews/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-za*&h98c5v#ko8a5ti1=a!1+#qwh!-bj-m31jy_899r3+f^fbk'
+SECRET_KEY = config.SECRET_KEY
+OPENAI_API_KEY = config.OPENAI_API_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
